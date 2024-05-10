e52aa477dbec49456f709eec9e5fed407f8ad587
lionel-kg <lionel77350@gmail.com>
2024-05-11 00:17:26 +0200

    fix

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -68,6 +68,7 @@ const Projects = () => {
           title="Airbnb"
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
           style={{ opacity: opacity }}
+          url="https://portfolio.lionelkg.com/movies"
         />
       </div>
     </div>
